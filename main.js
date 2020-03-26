f007f2595c61ec497aa64ac2a2884e9b70264425
AliceCZheng <alizheng97@gmail.com>
2020-03-26 14:32:04 -0400

    first screen done

--- main.js
+++ main.js
@@ -2,18 +2,71 @@ var randomCodeArray = [[1,2,3],[1,2,4],[1,3,2],[1,3,4],[1,4,2],[1,4,3],[2,1,3],[
 var numCodes = 24;
 var wordArray = [];
 
+var teamBoolean =0;
+var roomBoolean = 0;
+
+
+
+
+
+
+
 
 
 function teamName() {  
     var teamNameInput = document.getElementById("team-name-input");
     var teamName = document.getElementById("team-name");
     teamName.innerHTML = teamNameInput.value;
+
+    if (teamNameInput.value != "") {
+        teamBoolean += 1;
+    } else {
+        teamBoolean = 0;
+    }
+    if ((teamBoolean>0)&& (roomBoolean>0)) {
+        document.getElementById("continue-button").disabled = false;
+    } else {
+        document.getElementById("continue-button").disabled = true;
+    }
 }
 
 function roomCombo() {  
     var roomComboInput = document.getElementById("room-combo-input");
     var roomCombo = document.getElementById("room-combo");
     roomCombo.innerHTML = roomComboInput.value;
+    
+    if (roomComboInput.value != "") {
+        roomBoolean += 1;
+    }  else {
+        roomBoolean = 0;
+    }
+    if ((teamBoolean>0)&& (roomBoolean>0)) {
+        document.getElementById("continue-button").disabled = false;
+    } else {
+        document.getElementById("continue-button").disabled = true;
+    }
+}
+
+
+
+function onDisplay() {
+    var myVar;
+    var i;
+    for (i = 100; i>0 ; i--) {
+         document.getElementById("mainbox").style.opacity = (i/100);
+        
+    }
+ 
+}
+
+
+
+setTimeout(function() {
+    b = a + 4;
+}, (3 * 1000));
+
+function lowerOpacity(i) {
+    document.getElementById("mainbox").style.opacity = i/100;
 }
 
 
